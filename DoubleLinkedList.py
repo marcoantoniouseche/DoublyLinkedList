26d58557be312f7f2a3e7b289032d6ee0f94ad8d
Marco Useche <marcouseche@protonmail.com>
2021-09-06 07:07:18 -0400

    I did a class named DoublyLinkedList

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -1,4 +1,4 @@
-class DoublyLinkedList:
+class Node:
     def __init__(self, value, next_node=None, previous_node=None):
         self.value = value
         self.next_node = next_node
@@ -22,3 +22,27 @@ class DoublyLinkedList:
     def setPrevious_Node(self, previous_node):
         self.previous_node = previous_node
 
+class DoublyLinkedList:
+    def __init__(self, value):
+        self.current_node = Node(value)
+        self.initial_node = self.current_node
+
+    def add_nlink(self, value):
+        new_node = Node(value)
+        self.current_node.setNext_Node(new_node)
+        new_node.setPrevious_Node(self.current_node)
+        self.current_node = new_node
+
+    def printdl(self):
+        output = "[ "
+        currentNode = self.initial_node 
+        while currentNode:
+            output += str(currentNode.getValue())+", "
+            currentNode = currentNode.getNext_Node()
+        output += "]"
+        print(output)
+
+obj1 = DoublyLinkedList(4)
+obj1.add_nlink(5)
+obj1.add_nlink(7)
+obj1.printdl()
